4d096881ccce59bb889de1433f4409f741d9764f
ismael escalante <escalantedevjose@gmail.com>
2023-06-02 15:56:15 -0300

    modificar - eliminar marcas

--- resources/js/Pages/Marcas/Partials/Grid.vue
+++ resources/js/Pages/Marcas/Partials/Grid.vue
@@ -2,18 +2,31 @@
 import { Inertia } from '@inertiajs/inertia'
 import CircleButton from '../../../Components/CircleButton.vue'
 import { hasPermission } from '../../../Helpers/permissions'
+import Modal from '../../../Components/Modal.vue'
+import { reactive } from 'vue'
 
 const props = defineProps({
   marcas: Array
 })
 
-const update = (userId) => {
-  Inertia.get(`/users/${userId}/edit`)
+const update = (marcaId) => {
+  Inertia.get(`/marcas/${marcaId}/edit`)
 }
 
-const accesos = (userId) => {
-  Inertia.get(`/users/${userId}/accesos`)
+const modal = reactive({
+  show: false,
+  item: null
+})
+const showModal = (show = false, marca = null) => {
+  modal.show = show
+  modal.item = marca
 }
+
+const destroy = () => {
+  Inertia.delete(route('marcas.destroy', { marca: modal.item.id }))
+  showModal()
+}
+
 </script>
 
 <template>
@@ -32,21 +45,60 @@ const accesos = (userId) => {
       >
         <CTableDataCell>
           <CircleButton
-            title="Permission"
-            >
-            <span class="fa-solid fa-sitemap"></span>
-          </CircleButton>
-          <CircleButton
             class="ms-1"
             title="Modificar"
+            v-if="hasPermission($page, 'marcas.edit')"
+            @click="update(marca.id)"
           >
             <span class="fa-solid fa-pen-to-square"></span>
+          </CircleButton>
+          <CircleButton
+            v-if="hasPermission($page, 'marcas.destroy')"
+            class="ms-1"
+            title="Eliminar"
+            @click="showModal(true, marca)"
+          >
+            <span class="fa-solid fa-trash-can"></span>
           </CircleButton>
         </CTableDataCell>
         <CTableDataCell>{{ marca.nombre }}</CTableDataCell>
       </CTableRow>
     </CTableBody>
   </CTable>
+
+  <Modal
+    :visible="modal.show"
+    @close="showModal()"
+    >
+      <template #header>
+        Eliminar Marca
+      </template>
+      <CAlert color="warning">
+        <span>
+          <i class="fa-solid fa-trash-can m-2"></i>¿Estás seguro de deseas eliminar la marca?
+        </span>
+      </CAlert>
+      <CRow class="d-flex flex-column text-center">
+        <CCol>Marca: {{ modal.item.marca }}</CCol>
+      </CRow>
+      <template #footer>
+        <CButton
+          color="danger"
+          shape="rounded-pill"
+          class="text-white"
+          @click="destroy"
+        >
+          Eliminar
+        </CButton>
+        <CButton
+          color="secondary"
+          shape="rounded-pill"
+          @click="showModal()"
+        >
+          Cancelar
+        </CButton>
+      </template>
+  </Modal>
 
   <CAlert v-if="props.marcas === 0" color="info">
     Sin resultados.
